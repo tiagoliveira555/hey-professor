1c9b6b25d72a248763105f66ea692f78af3edc89
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-12-18 17:35:43 -0300

    HEY-8: adding dark mode option in navigation menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         "./node_modules/flowbite/**/*.js",
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
