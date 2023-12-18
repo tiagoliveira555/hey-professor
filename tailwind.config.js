fbde3f699d0e4732b7b39dc4726b68985e420917
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-12-18 17:27:34 -0300

    HEY-8: installing flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        "./node_modules/flowbite/**/*.js",
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
@@ -17,5 +18,8 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        require('flowbite/plugin'),
+        forms
+    ],
 };
