9c3cd478d6b1d5ecf8dd503a526b561e0e60dfb9
Tiago Oliveira <86539643+tiagoliveira555@users.noreply.github.com>
2023-12-19 12:02:15 -0300

    Revert "Feature/hey 11"

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -15,40 +16,34 @@ class User extends Authenticatable implements MustVerifyEmail
     use HasFactory;
     use Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password'          => 'hashed',
     ];
-
-    public function votes(): HasMany
-    {
-        return $this->hasMany(Vote::class);
-    }
-
-    public function like(Question $question): void
-    {
-        $this->votes()->updateOrCreate(
-            ['question_id' => $question->id],
-            [
-                'like'   => 1,
-                'unlike' => 0,
-            ]
-        );
-    }
-
-    public function unlike(Question $question): void
-    {
-        $this->votes()->updateOrCreate(
-            ['question_id' => $question->id],
-            [
-                'like'   => 0,
-                'unlike' => 1,
-            ]
-        );
-    }
 }
